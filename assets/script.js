6bbe825c6578f45031a64a129e34d46aa3795e07
Adrian <adrianribeiroprofissional@gmail.com>
2023-11-30 06:45:22 -0300

    terminei o responsivo do menu e iniciei a parte de seleção de cidade

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,15 @@
 function clicou(){
-    const ul = document.querySelector('ul');
-    const menufechado = document.querySelector('.menu-fechado');
-
-    if(ul.style.display == 'flex'){
-        ul.style.display = 'none';
+    const navegation = document.querySelector('.navegation');
+    
+    if(navegation.style.display == 'flex'){
+        navegation.style.display = 'none';
     } else {
-        ul.style.display = 'flex';
+        navegation.style.display = 'flex';
     }
 
-    if(menufechado.style.width == "170px"){
-        menufechado.style.width = "70px";
-    } else {
-        menufechado.style.width = "170px";
-    }
 
-}
+};
 
-
-const menu = document.querySelector('.header-mobile');
+const menu = document.querySelector('.menu-mobile');
 menu.addEventListener('click', clicou);
+
